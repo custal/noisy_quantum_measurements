84652ec6ecc02d6b958e92f333ff6a4b38f03c77
Chris Corlett <xl22293@bristol.ac.uk>
2023-07-12 16:24:03 +0100

    Refactor code to make data generation class POVMProtocolCalculator for more thorough data analysis and storage

---
+++ core.py
@@ -0,0 +1,160 @@
+import numpy as np
+import sympy as sp
+from sympy import Symbol
+from sympy.matrices import Matrix
+from sympy.physics.quantum import TensorProduct, Dagger
+from itertools import combinations
+from wolframclient.evaluation import WolframLanguageSession
+from tqdm import tqdm
+from datetime import datetime
+from uuid import uuid1
+import json
+from copy import deepcopy
+
+from utils import number_to_base, POVMClone, wolfram_kernel_path, basic_measure, eigen_value_measure_mathematica,\
+    dir_data
+
+
+class POVMProtocolCalculator:
+    a = Symbol("a")
+    b = Symbol("b")
+    time_format = '%Y_%m_%d-%H_%M_%S'
+
+    def __init__(self, number_of_clones: int, *povms: Matrix):
+        """ Set real to True if calculating using the basic measure """
+        self.id_ = None
+        self.date = None
+
+        self.povms = povms
+        self.number_of_measures = len(povms)
+        self.number_of_clones = number_of_clones
+        self.number_of_permutations = self.number_of_measures**self.number_of_clones
+        self.calculate_permutations()
+        self.number_of_protocols = 2**self.number_of_permutations
+        self.calculate_all_protocols()
+        self.lowest_measures = None
+
+    def calculate_permutations(self):
+        """ Calculate all permutations of the possible measurement outcomes for the given number of clones along with
+        the associated povm operator and probabilities for a general state. """
+
+        self.permutations = {}
+
+        for i in range(self.number_of_permutations):
+            permutation = tuple(number_to_base(i, self.number_of_measures, self.number_of_permutations))
+            tensor_povm = []
+            for j in permutation:
+                tensor_povm.append(self.povms[j])
+
+            tensor_povm = TensorProduct(*tensor_povm)
+            probability = self.calculate_probability(tensor_povm)
+            self.permutations[permutation] = POVMClone(tensor_povm, probability)
+
+    def calculate_all_protocols(self):
+        """ Find all possible combinations of the permutations """
+
+        # We only need half because the final half is identitcal to the first half.
+        # Eg. for 9 perms, taking groups of 2 and 7 is the same as taking groups of 7 and 2
+        max_len = int(self.number_of_permutations / 2)
+
+        permutations = list(self.permutations.keys())
+        # Add empty set edge case
+        self.protocols = {frozenset(): None, frozenset(permutations): None}
+
+        for i in range(max_len):
+            combs = combinations(permutations, i + 1)
+            for selection in combs:
+                duplicate_permutations = set(permutations)
+                for outcome in selection:
+                    duplicate_permutations.remove(outcome)
+
+                self.protocols[frozenset(selection)] = None
+                self.protocols[frozenset(duplicate_permutations)] = None
+
+    def calculate_probability(self, povm):
+        """ Find the probability of a generalised state. Using the column number of the povm is a bit hacky but should
+        be fine"""
+        psi = Matrix([0] * povm.cols)
+        psi[0] = self.a
+        psi[-1] = self.b
+
+        return (Dagger(psi) * povm * psi)[0]
+
+    def calculate_measures_sympy(self, measure_func=basic_measure):
+        print(f"Calculating {self.number_of_protocols} measures...")
+
+        for (key, item) in tqdm(self.protocols.items()):
+            measure = measure_func(key, self)
+            self.protocols[key] = measure
+
+        self.calculate_lowest_measures()
+        print("Done!")
+
+    def calculate_measures_mathematica(self, measure_func=eigen_value_measure_mathematica):
+        print(f"Calculating {self.number_of_protocols} measures...")
+
+        with WolframLanguageSession(wolfram_kernel_path) as session:
+            for i, (key, item) in tqdm(self.protocols.items()):
+                measure = measure_func(key, self, session)
+                self.protocols[key] = measure
+
+        self.calculate_lowest_measures()
+        print("Done!")
+
+    def calculate_lowest_measures(self):
+        lowest_val = np.inf
+        for key, item in self.protocols.items():
+            if item.measure_value <= lowest_val:
+                if item.measure_value == lowest_val:
+                    lowest_measures.append(item)
+                else:
+                    lowest_measures = [item]
+                lowest_val = item.measure_value
+
+        self.lowest_measures = lowest_measures
+
+    def save(self, file_name: str = None, dir: str = dir_data, mode: str = "a"):
+        """ Save the data to a file. We don't pickle the entire object as it isn't
+            conducive to long term storage ie. if we ever want to update this class. JSON is better for this.
+        """
+        # Generate a new id and date for each save as any changes made should be stored as a unique circuit
+        self.id_ = str(uuid1())
+        self.date = datetime.now()
+
+        if file_name is None:
+            file_name = f"{self.date.strftime(self.time_format)}-{self.id_}"
+
+        save_dict = deepcopy(self.__dict__)
+
+        with open(dir/file_name, mode) as f:
+            f.write(str(save_dict))
+            f.write("\n")
+
+    @classmethod
+    def load(cls, json_string: str):
+        """
+        WORK IN PROGRESS
+        Create a RandomCircuit object from a json string generated by the save method"""
+        load_dict = json.loads(json_string)
+        # Decode objects which couldn't be JSONified
+        load_dict["date"] = datetime.strptime(load_dict["date"], POVMProtocolCalculator.time_format)
+        load_dict["state"] = list(map(complex, load_dict["state"]))
+        load_dict["circuit"] = load_dict["circuit"]
+
+        return cls(**load_dict)
+
+
+if __name__ == "__main__":
+    phi0 = Matrix([1, 0])
+    phi1 = Matrix([sp.nsimplify(1/2), sp.nsimplify(sp.sqrt(3)/2)])
+    phi2 = Matrix([sp.nsimplify(1/2), sp.nsimplify(-sp.sqrt(3)/2)])
+
+    M0 = sp.nsimplify(2 / 3) * phi0 * Dagger(phi0)
+    M1 = sp.nsimplify(2 / 3) * phi1 * Dagger(phi1)
+    M2 = sp.nsimplify(2 / 3) * phi2 * Dagger(phi2)
+
+    povm = POVMProtocolCalculator(2, M0, M1, M2)
+
+    povm.calculate_measures_sympy()
+
+    povm.save("test.txt")
---
+++ utils.py
@@ -0,0 +1,173 @@
+import numpy as np
+from dataclasses import dataclass
+from sympy.matrices import Matrix
+from sympy import Symbol, mathematica_code, sqrt
+import sympy as sp
+from pathlib import Path
+from wolframclient.language import wlexpr
+import inspect
+
+wolfram_kernel_path = Path('C:\\Program Files\\Wolfram Research\\Mathematica\\13.2\\WolframKernel.exe')
+dir_data = Path(__file__).parents[0]/"data"
+
+@dataclass
+class ProtocolResult:
+    """Class for keeping track of protocol results """
+    protocol: frozenset
+    measure_function: str
+    measure_value: float
+
+@dataclass
+class EigenValueProtocolResult(ProtocolResult):
+    eigen_values: tuple
+    x_start_y_start: tuple
+
+@dataclass
+class POVMClone:
+    """ Class for keeping track of povms once cloned and associated probabilities """
+    povm: Matrix
+    probability: Symbol
+
+def dataclass_to_dict(dclass):
+    dclass = dclass.__dict__
+    for key, item in dclass.items():
+        if isinstance(item, Matrix):
+            dclass[key] = matrix_to__list(item)
+        elif isinstance(item, Symbol):
+            dclass[key] = str(item)
+
+    return dclass
+
+def matrix_to__list(mat: Matrix):
+    return mat.__array__().tolist()
+
+def number_to_base(n, b, pack):
+    """ n: Number in decimal
+        b: Convert number to base b
+        pack: Pack the number with zeros up to this value
+    """
+    if pack < n:
+        raise ValueError(f"pack ({pack}) must be greater than n ({n})")
+
+    num_of_digits = int(np.ceil(np.emath.logn(b, pack)))
+    digits = [0]*num_of_digits
+    i = num_of_digits-1
+    while n:
+        digits[i] = int(n % b)
+        n //= b
+        i -= 1
+    return digits
+
+def matrix_to_braket(matrix, qubit_num=None):
+    cols = matrix.cols
+    rows = matrix.rows
+
+    def binary(x):
+        if qubit_num is None:
+            return x
+        else:
+            num = f"{x:0>b}"
+            return (qubit_num - len(num)) * '0' + num
+
+    col_symbols = [sp.Symbol(f"|{binary(i)}>", commutative=False) for i in range(cols)]
+    row_symbols = [sp.Symbol(f"<{binary(i)}|", commutative=False) for i in range(rows)]
+
+    expression = 0
+
+    for i, element in enumerate(matrix):
+        row = i // cols
+        col = i % cols
+
+        expression += element * col_symbols[col] * row_symbols[row]
+
+    return expression
+
+
+#######################################################################################################################
+
+def eigen_value_measure_mathematica(protocol, povm_calculator, session):
+    permutations = povm_calculator.permutations
+    dim = permutations[0].rows
+    povm_sum = sp.zeros(dim)
+    for permutation in protocol:
+        povm_sum += permutations[permutation].povm
+
+    command = 'N[Eigenvalues[' + mathematica_code(povm_sum) + ']]'
+    eigen_vals = session.evaluate(wlexpr(command))
+
+    max_eig = max(eigen_vals)
+    min_eig = min(eigen_vals)
+
+    if max_eig <= 1 - 2 * min_eig:
+        (x, y) = (max_eig, (1 - max_eig) / 2)
+    elif 1 - 2 * min_eig <= max_eig and max_eig <= 1 - min_eig / 2:
+        (x, y) = (max_eig, min_eig)
+    elif 1 - min_eig / 2 <= max_eig:
+        (x, y) = ((1 - min_eig) / 2, min_eig)
+    else:
+        raise ValueError(f"Protocol {protocol} failed.")
+
+    measure = np.sqrt(1 / 3 * ((1 - x - y) ** 2 + (1 - x) * y))
+
+    return EigenValueProtocolResult(protocol, inspect.currentframe().f_code.co_name, measure, eigen_vals, (x, y))
+
+
+def integral_rms_measure_mathematica(protocol, povm_calculator, session):
+    a = povm_calculator.a
+    b = povm_calculator.b
+    permutations = povm_calculator.permutations
+
+    probability = 0
+    for permutation in protocol:
+        probability += permutations[permutation].probability
+
+    theta = sp.Symbol("θ", real=True)
+    phi = sp.Symbol("𝜙", real=True)
+
+    prob = probability - a * sp.conjugate(a)
+    prob = prob.subs({a: sp.cos(theta / 2), b: sp.E ** (sp.I * phi) * sp.sin(theta / 2)})
+
+    command = sqrt(sp.Integral(sp.sin(theta) / (4 * sp.pi) * prob ** 2, (theta, 0, sp.pi), (phi, 0, 2 * sp.pi)))
+    command = 'N[ReleaseHold[' + mathematica_code(command) + ']]'
+    measure = session.evaluate(wlexpr(command))
+
+    return ProtocolResult(protocol, inspect.currentframe().f_code.co_name, measure)
+
+def simplified_root_mean_square_measure(protocol, povm_calculator):
+    a = povm_calculator.a
+    b = povm_calculator.b
+    permutations = povm_calculator.permutations
+
+    probability = 0
+    for permutation in protocol:
+        probability += permutations[permutation].probability
+
+    x = probability.subs({a: 1, b: 0}).evalf()
+    y = probability.subs({a: 0, b: 1}).evalf()
+
+    p_plus = probability.subs({a: 1 / sqrt(2), b: 1 / sqrt(2)}).evalf()
+    p_minus = probability.subs({a: 1 / sqrt(2), b: -1 / sqrt(2)}).evalf()
+    p_iplus = probability.subs({a: 1 / sqrt(2), b: sp.I / sqrt(2)}).evalf()
+    p_iminus = probability.subs({a: 1 / sqrt(2), b: -sp.I / sqrt(2)}).evalf()
+    z = 0.5 * ((p_plus - p_minus) - sp.I * (p_iplus - p_iminus)).evalf()
+
+    measure = sqrt(1 / 3 * ((1 - x - y) ** 2 + (1 - x) * y + z * sp.conjugate(z)))
+    measure = sp.simplify(measure)
+
+    return ProtocolResult(protocol, inspect.currentframe().f_code.co_name, measure.evalf(chop=True))
+
+def basic_measure(protocol, povm_calculator):
+    """ Will need to set a and b sympy symbols to real if you want this to solve in any reasonable time scale """
+    a = povm_calculator.a
+    b = povm_calculator.b
+    permutations = povm_calculator.permutations
+
+    probability = 0
+    for permutation in protocol:
+        probability += permutations[permutation].probability
+
+    prob = probability - a * sp.conjugate(a)
+    prob = sp.simplify(prob.subs(b, sqrt(1 - a * sp.conjugate(a))).evalf())
+    measure = sqrt(sp.Integral(prob ** 2, (a, 0, 1)))
+
+    return ProtocolResult(protocol, inspect.currentframe().f_code.co_name, measure.evalf(chop=True))
